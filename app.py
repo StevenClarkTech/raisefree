34d7567c2342cfd34ab934362288d06ad1543c12
anthonymichaelclark <anthonymichaelclark1@gmail.com>
2017-10-19 20:23:09 -0500

    added the minimal amount of code to deploy app to web

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+from functools import wraps
+
+app = Flask(__name__)
+
+app.secret_key = 'lj)0di6i8jop#7$hk&)a9%92@0n6kf96jjm4m2p^j6h^(o(%+7'
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+
+#start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
