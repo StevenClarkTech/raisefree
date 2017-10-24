590920290641e0b0274d9c6636588c0e0489f353
Steven Petteruti <alfredpetteruti@Alfreds-Mac-mini.local>
2017-10-23 22:13:44 -0400

    added dashboard to url accessible

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app.secret_key = 'lj)0di6i8jop#7$hk&)a9%92@0n6kf96jjm4m2p^j6h^(o(%+7'
 def home():
 	return render_template('index.html')
 
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
 
 #start the server with the 'run()' method
 if __name__ == '__main__':
